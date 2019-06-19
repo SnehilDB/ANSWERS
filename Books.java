1e0a0d25e475aa97ae2c4819baf47f1ac010d982
SnehilDB <51693981+SnehilDB@users.noreply.github.com>
2019-06-20 01:09:35 +0530

    sorted according to author

--- Books.java
+++ Books.java
@@ -1,4 +1,4 @@
-package BOOKS;
+package yep;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,5 +6,60 @@ import java.util.Comparator;
 
 public class Books implements Comparable<Books>
 {
+    private int id;
+    private String name;
+    private String author;
+
+    public int compareTo(Books b)
+    {
+        return this.id- b.id ;
+
+    }
+    public Books(int id,String name,String author)
+    {
+        this.id=id;
+        this.name=name;
+        this.author=author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+class Author implements Comparator<Books>
+{
+    public int compare(Books m1, Books m2)
+    {
+        return m1.getAuthor().compareTo(m2.getAuthor());
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Books> list = new ArrayList<Books>();
+        list.add(new Books(1, "AAAA", "AAAAA"));
+        list.add(new Books(2, "BBBB", "BBBBB"));
+        list.add(new Books(3, "CCCC", "CCCCC"));
+        list.add(new Books(4, "DDDD", "DDDDD"));
+        System.out.println("\nSorted by author");
+        Author a = new Author();
+        Collections.sort(list, a);
+        for (Books b: list)
+            System.out.println(b.getName() + " " +
+                    b.getId() + " " +
+                    b.getAuthor());
+
+
+    }
 
 }

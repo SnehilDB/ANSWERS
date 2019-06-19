492002662e470748224821b2519f970d43d7cbd8
SnehilDB <51693981+SnehilDB@users.noreply.github.com>
2019-06-20 01:38:59 +0530

    search by name and author

--- General.java
+++ General.java
@@ -1,121 +1,46 @@
 package BOOKS;
 
 import java.awt.print.Book;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.*;
-import java.time.temporal.ChronoUnit;
-
-//import static java.time.temporal.ChronoUnit.DAYS;
+import java.util.HashMap;
+import java.util.Map;
 
 public class General
 {
-    Date fdate_issue;
-    Date fdate_till;
-    public int Book_id;
-    public String Book_name;
-    public String Book_issued_to;
-    public String Book_issue_date;
-    public String Book_till_date;
-    public long fine;
-    public long noOfDaysBetween;
-
-   public General(int id,String Book_name,String Book_issued_to,String Book_issue_date,String Book_till_date)
-   {
-    this.Book_id=id;
-    this.Book_name=Book_name;
-    this.Book_issued_to=Book_issued_to;
-    this.Book_issue_date=Book_issue_date;
-    this.Book_till_date=Book_till_date;
-
-
-   }
-
-    public void format_date(String Book_issue_date,String Book_till_date) throws Exception
-    {
-       // SimpleDateFormat formatter=new SimpleDateFormat("dd/MM/yyyy");
-        //fdate_issue=formatter.parse(Book_issue_date);
-        //fdate_till=formatter.parse(Book_till_date);
-
-        LocalDate fdate_issue = LocalDate.parse(Book_issue_date);
-        LocalDate fdate_till = LocalDate.parse(Book_till_date);
-
-        this.noOfDaysBetween = ChronoUnit.DAYS.between(fdate_issue,fdate_till);
-        System.out.println((noOfDaysBetween));
-
+    int id;
+    String name,author,publisher;
+    int quantity;
+    public General(int id, String name, String author, String publisher, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.quantity = quantity;
     }
-    public long fine()
-
-    {
+}
 
 
+class MapExample {
+    public static void main(String[] args) {
+        //Creating map of Books
+        Map<String,General> map=new HashMap<String, General>();
+        //Creating Books
+         General b1=new General(101,"Let us C","Yashwant Kanetkar","BPB",8);
+        General b2=new General(102,"Data Communications & Networking","Forouzan","Mc Graw Hill",4);
+        General b3=new General(103,"Operating System","Galvin","Wiley",6);
+        //Adding Books to map
+        map.put(b1.name,b1);
+        map.put(b2.name,b2);
+        map.put(b3.name,b3);
 
-        if((noOfDaysBetween-10)>30)
-        {
-         fine=(noOfDaysBetween-10)*30;
+        //Traversing map
+        for(Map.Entry<String, General> entry:map.entrySet()){
+            String key=entry.getKey();
+            General b=entry.getValue();
+            System.out.println(key+" Details:");
+            System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);
         }
-        else if(noOfDaysBetween-20>20)
-        {
-         fine=(noOfDaysBetween-20)*20;
-        }
-        else if(noOfDaysBetween-10>10)
-        {
-         fine=(noOfDaysBetween-10)*10;
-        }
-        else
-        {
-            System.out.println("no fine");
-        }
-
-        return fine;
-
     }
-
-
-
 }
-class Book_details
-{
-
-    public static void main(String[] args)
-        {
-          try {
-
-              Scanner sc = new Scanner(System.in);
-              System.out.println("Enter details");
-              // String  vid=sc.next();
-
-              Integer id = sc.nextInt();//Integer.parseInt(vid);
-              String Book_name = sc.next();
-              String Book_issued_to = sc.next();
-              String Book_issue_date = sc.next();
-              String Book_till_date = sc.next();
-
-
-              General Book1 = new General(id, Book_name, Book_issued_to, Book_issue_date, Book_till_date);
-              Book1.format_date(Book_issue_date, Book_till_date);
-              System.out.println(Book1.fine());
-
-              HashMap<Integer, General> hm = new HashMap<>();
-              General b1 = new General(1, "A", "AA", "2017-07-29", "2017-09-29");
-              General b2 = new General(2, "B", "BB", "2017-09-01", "2017-09-29");
-              hm.put(1, b1);
-              hm.put(2, b2);
-
-              for (Map.Entry<Integer, General> entry : hm.entrySet()) {
-                  int key = entry.getKey();
-                  General b = entry.getValue();
-                  System.out.println(key + " Details:");
-                  System.out.println(b.Book_id + " " + b.Book_name + " " + b.Book_issued_to + " " + b.Book_issue_date + " " + b.Book_till_date);
-
-              }
-          }
-
-              catch (Exception e){
-              e.printStackTrace();
-          }
 
 
 
-        }
-}

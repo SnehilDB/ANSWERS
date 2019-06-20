237ccfe1430980a9822cb590c19b83d75ed3328b
SnehilDB <51693981+SnehilDB@users.noreply.github.com>
2019-06-20 10:13:28 +0530

    COMPARABLE AND COMPARATOR NEW

--- Books.java
+++ Books.java
@@ -1,123 +1,65 @@
-package BOOKS;
+package yep;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-//import static sun.java2d.loops.CompositeType.General;
-
-public class Books {
-
-    Date fdate_issue;
-    Date fdate_till;
-    public int Book_id;
-    public String Book_name;
-    public String Book_issued_to;
-    public String Book_issue_date;
-    public String Book_till_date;
-    public long fine;
-    public long noOfDaysBetween;
-
-    public Books(int id, String Book_name, String Book_issued_to, String Book_issue_date, String Book_till_date)
-    {
-        this.Book_id=id;
-        this.Book_name=Book_name;
-        this.Book_issued_to=Book_issued_to;
-        this.Book_issue_date=Book_issue_date;
-        this.Book_till_date=Book_till_date;
-
-
-    }
-
-    public void format_date(String Book_issue_date,String Book_till_date) throws Exception
-    {
-        // SimpleDateFormat formatter=new SimpleDateFormat("dd/MM/yyyy");
-        //fdate_issue=formatter.parse(Book_issue_date);
-        //fdate_till=formatter.parse(Book_till_date);
-
-        LocalDate fdate_issue = LocalDate.parse(Book_issue_date);
-        LocalDate fdate_till = LocalDate.parse(Book_till_date);
-
-        this.noOfDaysBetween = ChronoUnit.DAYS.between(fdate_issue,fdate_till);
-        System.out.println((noOfDaysBetween));
-
-    }
-    public long fine()
-
-    {
-
-
-
-        if((noOfDaysBetween-10)>30)
-        {
-            fine=(noOfDaysBetween-10)*30;
-        }
-        else if(noOfDaysBetween-20>20)
-        {
-            fine=(noOfDaysBetween-20)*20;
-        }
-        else if(noOfDaysBetween-10>10)
-        {
-            fine=(noOfDaysBetween-10)*10;
-        }
-        else
-        {
-            System.out.println("no fine");
-        }
-
-        return fine;
-
-    }
-
-
-
-}
-class Book_details
+public class Books implements Comparable<Books>
 {
+    private int id;
+    private String name;
+    private String author;
 
+    public int compareTo(Books b)
+    {
+        return this.id- b.id ;
+
+    }
+    public Books(int id,String name,String author)
+    {
+        this.id=id;
+        this.name=name;
+        this.author=author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+class Author implements Comparator<Books>
+{
+    public int compare(Books m1, Books m2)
+    {
+        return m1.getAuthor().compareTo(m2.getAuthor());
+    }
+}
+
+class Main
+{
     public static void main(String[] args)
     {
-        try {
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter details");
-            // String  vid=sc.next();
-
-            Integer id = sc.nextInt();//Integer.parseInt(vid);
-            String Book_name = sc.next();
-            String Book_issued_to = sc.next();
-            String Book_issue_date = sc.next();
-            String Book_till_date = sc.next();
-
-
-            Books Book1 = new Books(id, Book_name, Book_issued_to, Book_issue_date, Book_till_date);
-            Book1.format_date(Book_issue_date, Book_till_date);
-            System.out.println(Book1.fine());
-
-            HashMap<Integer, Books> hm = new HashMap<>();
-            Books b1 = new Books(1, "A", "AA", "2017-07-29", "2017-09-29");
-            Books b2 = new Books(2, "B", "BB", "2017-09-01", "2017-09-29");
-            hm.put(1, b1);
-            hm.put(2, b2);
-
-            for (Map.Entry<Integer, Books> entry : hm.entrySet()) {
-                int key = entry.getKey();
-                Books b = entry.getValue();
-                System.out.println(key + " Details:");
-                System.out.println(b.Book_id + " " + b.Book_name + " " + b.Book_issued_to + " " + b.Book_issue_date + " " + b.Book_till_date);
+        ArrayList<Books> list = new ArrayList<Books>();
+        list.add(new Books(1, "AAAA", "AAAAA"));
+        list.add(new Books(2, "BBBB", "BBBBB"));
+        list.add(new Books(3, "CCCC", "CCCCC"));
+        list.add(new Books(4, "DDDD", "DDDDD"));
+        System.out.println("\nSorted by author");
+        Author a = new Author();
+        Collections.sort(list, a);
+        for (Books b: list)
+            System.out.println(b.getName() + " " +
+                    b.getId() + " " +
+                    b.getAuthor());
 
 
-            }
-        }
+    }
 
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-
-    }}
-
+}
